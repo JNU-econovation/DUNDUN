9496827856269f937e9f2ddb89559b32c4aa084f
Jang Hong Jun <jhjun99421@gmail.com>
2023-07-03 16:52:23 +0900

    Feat : add button to move to the current location
    화면 우측 하단 버튼을 누르면 현재 내 위치로 이동하는 기능 추가

--- app/src/main/java/com/project981/dundun/view/map/MapFragment.kt
+++ app/src/main/java/com/project981/dundun/view/map/MapFragment.kt
@@ -1,15 +1,30 @@
 package com.project981.dundun.view.map
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
 import com.project981.dundun.R
-import com.project981.dundun.databinding.FragmentHomeBinding
 import com.project981.dundun.databinding.FragmentMapBinding
+import net.daum.mf.map.api.MapView
+
 
 class MapFragment : Fragment() {
+    private val ACCESS_FINE_LOCATION = 1000
+    private var mapView : MapView? = null
     private var _binding : FragmentMapBinding? = null
     private val binding : FragmentMapBinding
         get() = requireNotNull(_binding)
@@ -18,16 +33,86 @@ class MapFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentMapBinding.inflate(layoutInflater)
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //do something
+
+        mapView = MapView(activity)
+        binding.mapView.addView(mapView)
+
+        binding.localButton.setOnClickListener {
+            if (checkLocationService()) {
+                permissionCheck()
+            }
+            else {
+                Toast.makeText(context, "GPS를 켜주세요", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        mapView = null
     }
+
+    private fun permissionCheck() {
+        val preference = activity?.getPreferences(Context.MODE_PRIVATE)
+        val isFirstCheck = preference?.getBoolean("isFirstPermissionCheck", true)
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            // 권한이 없는 상태
+            if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // 권한 거절
+                val builder = AlertDialog.Builder(context)
+                builder.setMessage("현재 위치를 확인하시려면 위치 권한을 허용해주세요.")
+                builder.setPositiveButton("확인") { dialog, which ->
+                    ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), ACCESS_FINE_LOCATION)
+                }
+                builder.setNegativeButton("취소") { dialog, which ->
+
+                }
+                builder.show()
+            }
+            else {
+                if (isFirstCheck == true) {
+                    // 최초 권한 요청
+                    preference.edit().putBoolean("isFirstPermissionCheck", false).apply()
+                    ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), ACCESS_FINE_LOCATION)
+                }
+                else {
+                    val builder = AlertDialog.Builder(context)
+                    builder.setMessage("현재 위치를 확인하시려면 설정에서 위치 권한을 허용해주세요.")
+                    builder.setPositiveButton("설정으로 이동") { dialog, which ->
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("com.project981.dundun"))
+                        startActivity(intent)
+                    }
+                    builder.setNegativeButton("취소") { dialog, which ->
+
+                    }
+                    builder.show()
+                }
+            }
+        }
+        // 권한 있는 상태
+        else {
+            startTracking()
+        }
+    }
+
+    // GPS 확인
+    private fun checkLocationService(): Boolean {
+        val locationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+    }
+
+    private fun startTracking() {
+        mapView?.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
+    }
+
 }
