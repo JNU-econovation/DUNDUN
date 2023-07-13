f48dd642a97eb8c999c4bf3e2eb0158919fe2a3e
K_Gs:D <rootachieve6053@gmail.com>
2023-07-13 16:53:56 +0900

    feat : 프래그먼트 및 액티비티 연결

--- app/src/main/java/com/project981/dundun/SignActivity.kt
+++ app/src/main/java/com/project981/dundun/SignActivity.kt
@@ -1,12 +1,25 @@
 package com.project981.dundun
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.project981.dundun.R
+import com.project981.dundun.view.signin.SigninViewModel
 
 class SignActivity : AppCompatActivity() {
+    private val viewModel: SigninViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign)
+
+        viewModel.isLogin.observe(this){
+            if(it){
+                val intent = Intent(this,MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
