cf3454d1c4ce9a9c298842aa622c364f6aca4a25
Jang Hong Jun <jhjun99421@gmail.com>
2023-07-21 16:39:06 +0900

    Feat : make floating action button for artist user at home fragment
    홈 화면에서 아티스트 유저들이 사용할 글쓰기와 프로필 화면으로 이동하는 플로팅 버튼 구현

--- app/src/main/java/com/project981/dundun/view/home/HomeFragment.kt
+++ app/src/main/java/com/project981/dundun/view/home/HomeFragment.kt
@@ -1,15 +1,25 @@
 package com.project981.dundun.view.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.material.animation.AnimationUtils
+import com.project981.dundun.R
 import com.project981.dundun.databinding.FragmentHomeBinding
-import com.project981.dundun.model.dto.NoticeDisplayDTO
 
 class HomeFragment : Fragment() {
     private var _binding : FragmentHomeBinding? = null
+
+    private val rotateOpen: Animation by lazy { android.view.animation.AnimationUtils.loadAnimation(requireContext(), R.anim.rotate_open_anim) }
+    private val rotateClose: Animation by lazy { android.view.animation.AnimationUtils.loadAnimation(requireContext(), R.anim.rotate_close_anim) }
+    private val fromBottom: Animation by lazy { android.view.animation.AnimationUtils.loadAnimation(requireContext(), R.anim.from_bottom_anim) }
+    private val toBottom: Animation by lazy { android.view.animation.AnimationUtils.loadAnimation(requireContext(), R.anim.to_bottom_anim) }
+    private var clicked = false
+
     private val binding : FragmentHomeBinding
         get() = requireNotNull(_binding)
     override fun onCreateView(
@@ -23,6 +33,56 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //do something
+
+        binding.iconNoticeAdd.setOnClickListener {
+            onAddButtonClicked()
+        }
+        binding.iconNoticeWrite.setOnClickListener {
+            Toast.makeText(requireContext(), "Write Notice", Toast.LENGTH_SHORT).show()
+
+        }
+        binding.iconNoticeProfile.setOnClickListener {
+            Toast.makeText(requireContext(), "My Profile", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun onAddButtonClicked() {
+        setVisibility(clicked)
+        setAnimation(clicked)
+        setClickable(clicked)
+        clicked = !clicked
+    }
+
+    private fun setVisibility(clicked: Boolean) {
+        if (!clicked) {
+            binding.iconNoticeWrite.visibility = View.VISIBLE
+            binding.iconNoticeProfile.visibility = View.VISIBLE
+        } else {
+            binding.iconNoticeWrite.visibility = View.INVISIBLE
+            binding.iconNoticeProfile.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun setAnimation(clicked: Boolean) {
+        if (!clicked) {
+            binding.iconNoticeWrite.startAnimation(fromBottom)
+            binding.iconNoticeProfile.startAnimation(fromBottom)
+            binding.iconNoticeAdd.startAnimation(rotateOpen)
+        } else {
+            binding.iconNoticeWrite.startAnimation(toBottom)
+            binding.iconNoticeProfile.startAnimation(toBottom)
+            binding.iconNoticeAdd.startAnimation(rotateClose)
+        }
+    }
+
+    private fun setClickable(clicked: Boolean) {
+        if (clicked) {
+            binding.iconNoticeWrite.isClickable = false
+            binding.iconNoticeProfile.isClickable = false
+        } else {
+            binding.iconNoticeWrite.isClickable = true
+            binding.iconNoticeProfile.isClickable = true
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/project981/dundun/view/home/NoticeAdapter.kt
+++ app/src/main/java/com/project981/dundun/view/home/NoticeAdapter.kt
@@ -10,37 +10,39 @@ import com.bumptech.glide.Glide
 import com.project981.dundun.R
 import com.project981.dundun.databinding.NoticeItemBinding
 import com.project981.dundun.model.dto.NoticeDisplayDTO
+import java.text.SimpleDateFormat
 
-class NoticeAdapter(val noticeList: ArrayList<NoticeDisplayDTO>) : RecyclerView.Adapter<NoticeAdapter.CustomViewHolder>() {
+class NoticeAdapter(val noticeList: ArrayList<NoticeDisplayDTO>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+                                                                    // Adapter 안에 사용할 뷰홀더를 넣어야하는거 아닌지?
 
+    inner class NoticeViewHolder(val binding: NoticeItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: NoticeDisplayDTO) {
+            val dateFormat = "yyyy년 MM월 dd일 E요일 a h:m"
+            val date = item.date
+            val simpleDateFormat = SimpleDateFormat(dateFormat)
+            val calendarDate: String = simpleDateFormat.format(date)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoticeAdapter.CustomViewHolder {
-        return CustomViewHolder(NoticeItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+            binding.txtNoticeArtistName.text = item.artistName
+            binding.txtNoticeLocation.text = item.locationDescription
+            binding.txtNoticeDate.text = calendarDate
+            binding.txtNoticeContent.text = item.content
+            // createDate는 어떻게?
+            // binding.txtNoticeCreateAgo.text = item.createDate
+            binding.txtNoticeLikeCount.text = item.likeCount.toString()
+
+        }
+
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder /*NoticeAdapter.NoticeViewHolder*/ {
+        return NoticeViewHolder(NoticeItemBinding.inflate((LayoutInflater.from(parent.context))))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as NoticeViewHolder).bind(noticeList[position])
     }
 
     override fun getItemCount(): Int {
         return noticeList.size
     }
 
-    override fun onBindViewHolder(holder: NoticeAdapter.CustomViewHolder, position: Int) {
-//        holder.profileImageUrl.setImageResource(noticeList.get(position).profileImageUrl.toInt())
-//        holder.profileName.text = noticeList.get(position).artistName
-//        holder.poster.setImageResource(noticeList.get(position).contentImageURL.toInt())
-//        holder.notice_text.text = noticeList.get(position).content
-//        holder.likeCount.text = noticeList.get(position).likeCount.toString()
-        holder.bind(noticeList[position])
-    }
-
-    class CustomViewHolder(val binding: NoticeItemBinding) : RecyclerView.ViewHolder(binding.root){
-//        val profileImageUrl = itemView.findViewById<ImageView>(R.id.profile_imgage)    // 프로필 사진
-//        val profileName = itemView.findViewById<TextView>(R.id.profile_name)        // 프로필 이름
-//        val poster = itemView.findViewById<ImageView>(R.id.notice_poster)           // 포스터
-//        val notice_text = itemView.findViewById<TextView>(R.id.notice_text)         // 게시글 작성
-//        val likeImage = itemView.findViewById<ImageView>(R.id.notice_like)          // 좋아요 이미지
-//        val likeCount = itemView.findViewById<TextView>(R.id.notice_like_num)      // 좋아요 수
-
-        fun bind (item: NoticeDisplayDTO) {
-            //Glide.with(binding.profileImgage.context).load(R.drawable.profile).into()
-        }
-    }
 }
