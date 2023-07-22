878e73922b1f3ad2e02d04b2d7de9bba33459ee8
K_Gs:D <rootachieve6053@gmail.com>
2023-07-22 17:22:57 +0900

    feat : bottom adapter 수정

--- app/src/main/java/com/project981/dundun/view/map/BottomRecyclerAdapter.kt
+++ app/src/main/java/com/project981/dundun/view/map/BottomRecyclerAdapter.kt
@@ -3,30 +3,30 @@ package com.project981.dundun.view.map
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.project981.dundun.R
 import com.project981.dundun.databinding.BottomListItemBinding
-import com.project981.dundun.databinding.BottomsheetListBinding
-import com.project981.dundun.model.dto.MapDetailDTO
+import com.project981.dundun.model.dto.BottomDetailDTO
 import java.text.SimpleDateFormat
-import java.util.Date
+import java.util.Locale
 
-class MapRecyclerAdapter(val itemList: ArrayList<MapDetailDTO>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class BottomRecyclerAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private var itemList = listOf<BottomDetailDTO>()
 
-    inner class MapViewHolder(val binding: BottomListItemBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: MapDetailDTO){
+    inner class MapViewHolder(val binding: BottomListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: BottomDetailDTO) {
             val dateFormat = "yyyy.MM.dd a HH:mm"
             val date = item.date
-            val simpleDateFormat = SimpleDateFormat(dateFormat)
+            val simpleDateFormat = SimpleDateFormat(dateFormat, Locale.getDefault())
             val mapDate: String = simpleDateFormat.format(date)
 
-            binding.txtMapitemName.text = item.noticeTitle
+            binding.txtMapitemName.text = item.artistName
             binding.txtMapitemDate.text = mapDate
             binding.txtMapitemLocation.text = item.locationDescription
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return MapViewHolder(BottomListItemBinding.inflate(LayoutInflater.from(parent.context)))
+        return MapViewHolder(BottomListItemBinding.inflate(LayoutInflater.from(parent.context),parent,false))
     }
 
     override fun getItemCount(): Int {
@@ -35,5 +35,10 @@ class MapRecyclerAdapter(val itemList: ArrayList<MapDetailDTO>) : RecyclerView.A
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         (holder as MapViewHolder).bind(itemList[position])
+    }
+
+    fun setDate(list: List<BottomDetailDTO>) {
+        itemList = list
+        notifyDataSetChanged()
     }
 }
