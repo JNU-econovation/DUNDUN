3001b3333e9528c45db64cfc00404dc0bfabbd5a
Jang Hong Jun <jhjun99421@gmail.com>
2023-07-25 20:10:08 +0900

    Feature : 게시글 작성 레이아웃 구성 완료
    switch버튼 on/off로 캘린더와 맵 검색 레이아웃 접기/펼치기 구현.
    캘린더 datepicker와 timepicker 추가로 길이가 길어져 스크롤 뷰로 변경.

--- app/src/main/java/com/project981/dundun/view/artist/WriteNoticeFragment.kt
+++ app/src/main/java/com/project981/dundun/view/artist/WriteNoticeFragment.kt
@@ -1,15 +1,19 @@
 package com.project981.dundun.view.artist
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
 import androidx.fragment.app.Fragment
 import com.project981.dundun.databinding.FragmentMypageBinding
+import com.project981.dundun.databinding.FragmentNoticewriteBinding
+import java.util.Calendar
 
 class WriteNoticeFragment : Fragment() {
-    private var _binding : FragmentMypageBinding? = null
-    private val binding : FragmentMypageBinding
+    private var _binding : FragmentNoticewriteBinding? = null
+    private val binding : FragmentNoticewriteBinding
         get() = requireNotNull(_binding)
 
     override fun onCreateView(
@@ -17,13 +21,30 @@ class WriteNoticeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentMypageBinding.inflate(layoutInflater)
+        _binding = FragmentNoticewriteBinding.inflate(layoutInflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //do something
+        binding.switchCalender.setOnCheckedChangeListener{ _, onSwitch ->
+            if (!onSwitch) {
+                binding.layoutDetailCalender.visibility = View.GONE
+
+            } else {
+                binding.layoutDetailCalender.visibility = View.VISIBLE
+            }
+        }
+
+        binding.switchMap.setOnCheckedChangeListener{ _, onSwitch ->
+            if (!onSwitch) {
+                binding.layoutDetailMap.visibility = View.GONE
+
+            } else {
+                binding.layoutDetailMap.visibility = View.VISIBLE
+            }
+        }
     }
 
     override fun onDestroyView() {
